06f760a1db508b591696fbfb30cfd64b167400c0
Andrew Valencik <mightyhops@gmail.com>
2014-01-25 02:29:41 -0400

    Removed oninput, now entirely click based. Added two click modes, elements and collisions.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,20 +15,11 @@
         buildDefaultData(rows,columns);
         populateSquares();
 
-        //On square click, change current element
-        squares.find(".square").click(function(){
-          console.log("Clicked " + this.innerHTML);
-          currentElement = mapElements.indexOf(this.innerHTML);
-          this.innerHTML = mapElements[(currentElement + 1) % mapElements.length]
-          onInput();
-        });
+        //Attached onclick event to all squares
+        squares.find(".square").click(squareClick);
 
-        input.keyup(onInput);
-        method.change(onInput);
-
-
-        onInput({});
     }); //end document ready
+
 
     var dataElement = [];
 
@@ -42,74 +33,23 @@
     var displayContent;
 
     var methods = {
-        REGEX: 0,
-        OEIS: 3
+        ELEMENTS: 0,
+        COLLISIONS: 1
     };
 
-    function criteriaRegex(){
-        return {
-            type: 0,
-            input: "",
-            test: function(value){
-                return new RegExp(this.input).test(value);
-            }
-        }
-    }
 
-    function criteriaOEIS(){
-        return {
-            type: 1,
-            input: [],
-            test: function(value){
-                return $.inArray(parseFloat(value), this.input) !== -1;
-            }
-        }
-    }
+    function squareClick(){
+        console.log("Clicked " + this.innerHTML);
+          if (parseInt(method.val()) == methods.ELEMENTS) {
+            var currentElement = mapElements.indexOf(this.innerHTML);
+            this.innerHTML = mapElements[(currentElement + 1) % mapElements.length] }
+          else if (parseInt(method.val()) == methods.COLLISIONS) {
+            if ($(this).hasClass('hit0')) {
+              $(this).addClass('hit1').removeClass('hit0'); }
+            else if ($(this).hasClass('hit1')) {
+              $(this).addClass('hit0').removeClass('hit1'); }
+          }
 
-
-    var oesiTimeout;
-
-    function onInput(event){
-        var userInput = input.val();
-        if(userInput.length){
-            var criteria;
-            switch(parseInt(method.val())){
-                case methods.REGEX:
-                default:
-                    criteria = criteriaRegex();
-
-            }
-            criteria.input = userInput;
-            startVisualization(criteria);
-        }
-    }
-
-    function startVisualization(criteria){
-        try {
-            runVisualization(criteria);
-        } catch (e) {
-            message.addClass('invalid').text('invalid');
-            squares.find('.square').addClass('red').removeClass('green');
-        }
-    }
-
-    function runVisualization(criteria){
-        var matches = 0;
-        squares.find('.square').each(function(i){
-            var square = $(this);
-            square.addClass('red').removeClass('green');
-            function match(){
-                square.addClass('green').removeClass('red');
-                matches++;
-            }
-            var content = square.text();
-            var y = Math.floor(i / columns) + 1;
-            var x = (i % columns) + 1;
-            if(criteria.test(content, i, x, y)){
-                match();
-            }
-        });
-        message.removeClass('invalid').text(matches + ' matches');
     }
 
     function buildDefaultData(rows,columns){
